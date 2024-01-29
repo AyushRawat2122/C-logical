ba33cd552e228a2b0bf5f32f33772b83a1badbd7
Ayush <Balbeershingrawat@gmail.com>
2024-01-29 11:27:56 +0530

    assignment done

---
+++ assignment 1.c
@@ -0,0 +1,131 @@
+/* // question 1
+
+#include<stdio.h>
+int main (){
+    
+    printf("hello,welcome to jaipur");
+    return 0;
+}
+
+
+//question 2
+
+#include<stdio.h>
+int main (){
+    
+    printf("hi,how are you ");
+    return 0;
+}
+
+//question 3
+#include<stdio.h>
+int main(){
+    
+    printf("Hello world");
+    printf("how are you");
+    return 0;
+}
+
+//question  4
+ 
+#include<stdio.h>
+int main(){
+    printf("Neha\n");
+    printf("lshika\n");
+    printf("Nikhil\n");
+    printf("Araav\n");
+    printf("Ayush\n");
+    
+    return 0;
+    
+}
+
+//question 5
+#include<stdio.h>
+int main(){
+    
+    printf("Amsterdam\n");
+    printf("Slovenia\n");
+    printf("Mogolia\n");
+    printf("Hungary\n");
+    printf("Coulmbia\n");
+    
+    return 0;
+    
+}
+
+//question 6
+
+#include<stdio.h>
+int main(){
+    
+    printf("Las Vegas\n");
+    printf("Los Angeles\n");
+    printf("Queens\n");
+    printf("Manhattan\n");
+    printf("Ohio\n");
+    
+    return 0;
+    
+}
+
+//question  7
+
+#include<stdio.h>
+int main(){
+    
+    printf("Mango\n");
+    printf("Apple\n");
+    printf("Banana\n");
+    printf("Papaya\n");
+    printf("Watermelon\n");
+    
+    return 0;
+    
+}
+
+//question 8
+
+#include<stdio.h>
+int main(){
+    
+    printf("Pea\n");
+    printf("Potato\n");
+    printf("Ladyfinger\n");
+    printf("Capsicum\n");
+    printf("Spinach\n");
+    
+    return 0;
+    
+}
+
+//question 9
+
+#include<stdio.h>
+int main(){
+    
+    printf("Kimchi\n");
+    printf("Lasagna\n");
+    printf("Ramen\n");
+    printf("borruito\n");
+    printf("Bulgogi\n");
+    
+    return 0;
+    
+}
+
+//question 10
+*/
+#include<stdio.h>
+int main(){
+    
+    printf("Karnataka\n");
+    printf("Gujarat\n");
+    printf("Haryana\n");
+    printf("Rajasthan\n");
+    printf("Tamil Nadu\n");
+    
+    return 0;
+    
+}
+
