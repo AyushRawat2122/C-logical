582dd9a6b808b55380341e9e19b77b13951b6362
Ayush <Balbeershingrawat@gmail.com>
2024-02-12 21:33:59 +0530

    patterns done

---
+++ Assignment12.c
@@ -0,0 +1,337 @@
+//1 )
+
+#include <stdio.h>
+
+int main()
+{
+   for(int i = 1 ; i <= 5 ; i++)
+   {
+    for(int j = 1 ; j <= 5 ; j++)
+    {
+      if(i==j)
+      {
+        printf("*");
+      }
+      else
+      {
+        printf(" ");
+      }
+    }
+    printf("\n");
+   }
+
+    return 0;
+}
+
+//2 )
+
+#include <stdio.h>
+
+int main()
+{
+   for(int i = 1 ; i <= 5 ; i++)
+   {
+    for(int j = 1 ; j <= 4 ; j++)
+    {
+      if(j == 1 || j == 4 ||i == 1 ||i == 3)
+      {
+        printf("*");
+      }
+      else
+      {
+        printf(" ");
+      }
+    }
+    printf("\n");
+   }
+
+    return 0;
+}
+
+//3 )
+
+#include <stdio.h>
+
+int main()
+{
+   for(int i = 1 ; i <= 5 ; i++)
+   {
+    for(int j = 1 ; j <= 5 ; j++)
+    {
+      if(i==1 || j==1)
+      {
+        printf("*");
+      }
+      else
+      {
+        printf(" ");
+      }
+    }
+    printf("\n");
+   }
+
+    return 0;
+}
+
+//4 )
+
+#include <stdio.h>
+
+int main()
+{
+   for(int i = 1 ; i <= 5 ; i++)
+   {
+    for(int j = 1 ; j <= 5 ; j++)
+    {
+      if(i==1 || i==5 || j==1)
+      {
+        printf("*");
+      }
+      else
+      {
+        printf(" ");
+      }
+    }
+    printf("\n");
+   }
+
+    return 0;
+}
+
+//5 )
+
+#include <stdio.h>
+
+int main()
+{
+   for(int i = 1 ; i <= 5 ; i++)
+   {
+    for(int j = 1 ; j <= 5 ; j++)
+    {
+      if(i==1 || i==5 || j==5)
+      {
+        printf("*");
+      }
+      else
+      {
+        printf(" ");
+      }
+    }
+    printf("\n");
+   }
+
+    return 0;
+}
+
+//6 )
+
+#include <stdio.h>
+
+int main()
+{
+   for(int i = 1 ; i <= 5 ; i++)
+   {
+    for(int j = 1 ; j <= 5 ; j++)
+    {
+      if(i==1 || i==5 || j==1 || i == 3)
+      {
+        printf("*");
+      }
+      else
+      {
+        printf(" ");
+      }
+    }
+    printf("\n");
+   }
+
+    return 0;
+}
+
+// 7 )
+
+
+#include <stdio.h>
+
+int main()
+{
+   for(int i = 1 ; i <= 5 ; i++)
+   {
+    for(int j = 1 ; j <= 5 ; j++)
+    {
+      if(i==1 || i==5 || i == 3)
+      {
+        printf("*");
+      }
+      else if( i==2 && j==1)
+      {
+        printf("*");
+      }
+      else if( i==4 && j==5)
+      {
+        printf("*");
+      }
+      else
+      {
+        printf(" ");
+      }
+    }
+    printf("\n");
+   }
+
+    return 0;
+}
+
+//8 )
+
+
+#include <stdio.h>
+
+int main()
+{
+   for(int i = 1 ; i <= 5 ; i++)
+   {
+    for(int j = 1 ; j <= 5 ; j++)
+    {
+      if(i == 1 || i == 5 || j == 1 || j == 5)
+      {
+        printf("*");
+      }
+      else
+      {
+        printf(" ");
+      }
+    }
+    printf("\n");
+   }
+
+    return 0;
+}
+
+//9 )
+
+
+#include <stdio.h>
+
+int main()
+{
+   for(int i = 1 ; i <= 5 ; i++)
+   {
+    for(int j = 1 ; j <= 5 ; j++)
+    {
+      if(i == 1 || i == 5 || j == 1 || j == 5 || i == 3)
+      {
+        printf("*");
+      }
+      else
+      {
+        printf(" ");
+      }
+    }
+    printf("\n");
+   }
+
+    return 0;
+}
+
+//10 )
+
+#include <stdio.h>
+
+int main()
+{
+   for(int i = 1 ; i <= 5 ; i++)
+   {
+    for(int j = 1 ; j <= 5 ; j++)
+    {
+      if(i == 1 || j == 3 || i == 5)
+      {
+        printf("*");
+      }
+      else
+      {
+        printf(" ");
+      }
+    }
+    printf("\n");
+   }
+
+    return 0;
+}
+
+//11 )
+
+#include <stdio.h>
+
+int main()
+{
+   for(int i = 1 ; i <= 5 ; i++)
+   {
+    for(int j = 1 ; j <= 5 ; j++)
+    {
+      if(i == 1 || j == i || i == 5)
+      {
+        printf("*");
+      }
+      else
+      {
+        printf(" ");
+      }
+    }
+    printf("\n");
+   }
+
+    return 0;
+}
+
+//12 )
+
+#include <stdio.h>
+
+int main()
+{
+   for(int i = 1 ; i <= 5 ; i++)
+   {
+    for(int j = 1 ; j <= 5 ; j++)
+    {
+      if(i == 1 || j == i || j == 1)
+      {
+        printf("*");
+      }
+      else
+      {
+        printf(" ");
+      }
+    }
+    printf("\n");
+   }
+
+    return 0;
+}
+
+//13 )
+
+
+#include <stdio.h>
+
+int main()
+{
+   for(int i = 1 ; i <= 5 ; i++)
+   {
+    for(int j = 1 ; j <= 5 ; j++)
+    {
+      if(i == 1 || j == i || j == 1 || j == 5 || i == 5)
+      {
+        printf("*");
+      }
+      else
+      {
+        printf(" ");
+      }
+    }
+    printf("\n");
+   }
+
+    return 0;
+}
+
